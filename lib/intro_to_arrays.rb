acb5e0113fe3cac8d1e965b4f889db62c40e5ec3
Amari Ellington <amari.l.ellington@gmail.com>
2019-04-10 21:22:27 +0000

    Automatically backed up by Learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -15,3 +15,8 @@ def third_element(taylor_swift)
 taylor_swift = ["Welcome to New York", "Blank Space", "Style", "Out of The Woods"]
 return taylor_swift[2]
 end
+
+def last_element
+taylor_swift = ["Welcome to New York", "Blank Space", "Style", "Out of The Woods"]
+return taylor_swift[3]
+end
