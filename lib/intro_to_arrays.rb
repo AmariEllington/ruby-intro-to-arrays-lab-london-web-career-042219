53acf558a956ea67096d06c7774b695339bcf90c
Amari Ellington <amari.l.ellington@gmail.com>
2019-04-10 21:30:24 +0000

    Automatically backed up by Learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -26,7 +26,7 @@ def first_element_with_array_methods(south_east_asia)
   south_east_asia.shift
 end
 
-def last_element_with_array_methods
+def last_element_with_array_methods(south_east_asia)
   south_east_asia = ["Thailand", "Cambodia", "Singapore", "Myanmar"]
   south_east_asia.pop
 end
