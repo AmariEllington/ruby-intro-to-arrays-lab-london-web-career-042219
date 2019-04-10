e2b0b320f96d716301528542a04b11397efa57e8
Amari Ellington <amari.l.ellington@gmail.com>
2019-04-10 21:26:10 +0000

    Automatically backed up by Learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -23,5 +23,5 @@ end
 
 def first_element_with_array_methods(south_east_asia)
   south_east_asia = ["Thailand", "Cambodia", "Singapore", "Myanmar"]
-  south_east_asia["Thailand"]
+  return south_east_asia["Thailand"]
 end
